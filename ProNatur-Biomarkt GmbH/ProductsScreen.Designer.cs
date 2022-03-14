2af30f2f83c776abb1de58ac955ff3d7a5cd26c9
Holger Maerz <holger.maerz@nakade.de>
2022-03-14 16:24:33 +0100

    buttons und grid

---
+++ ProNatur-Biomarkt GmbH/ProductsScreen.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ProNatur_Biomarkt_GmbH
+{
+    partial class ProductsScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductsScreen));
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxMarke = new System.Windows.Forms.TextBox();
+            this.lblMarke = new System.Windows.Forms.Label();
+            this.lblKategorie = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblPreis = new System.Windows.Forms.Label();
+            this.comboBoxKategorie = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridViewProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblName.Location = new System.Drawing.Point(36, 22);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(57, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(131, 20);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(276, 22);
+            this.txtBoxName.TabIndex = 1;
+            // 
+            // txtBoxMarke
+            // 
+            this.txtBoxMarke.Location = new System.Drawing.Point(131, 60);
+            this.txtBoxMarke.Name = "txtBoxMarke";
+            this.txtBoxMarke.Size = new System.Drawing.Size(276, 22);
+            this.txtBoxMarke.TabIndex = 3;
+            // 
+            // lblMarke
+            // 
+            this.lblMarke.AutoSize = true;
+            this.lblMarke.BackColor = System.Drawing.Color.Transparent;
+            this.lblMarke.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarke.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblMarke.Location = new System.Drawing.Point(36, 62);
+            this.lblMarke.Name = "lblMarke";
+            this.lblMarke.Size = new System.Drawing.Size(60, 20);
+            this.lblMarke.TabIndex = 2;
+            this.lblMarke.Text = "Marke";
+            // 
+            // lblKategorie
+            // 
+            this.lblKategorie.AutoSize = true;
+            this.lblKategorie.BackColor = System.Drawing.Color.Transparent;
+            this.lblKategorie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKategorie.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblKategorie.Location = new System.Drawing.Point(36, 105);
+            this.lblKategorie.Name = "lblKategorie";
+            this.lblKategorie.Size = new System.Drawing.Size(89, 20);
+            this.lblKategorie.TabIndex = 4;
+            this.lblKategorie.Text = "Kategorie";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // lblPreis
+            // 
+            this.lblPreis.AutoSize = true;
+            this.lblPreis.BackColor = System.Drawing.Color.Transparent;
+            this.lblPreis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreis.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPreis.Location = new System.Drawing.Point(36, 143);
+            this.lblPreis.Name = "lblPreis";
+            this.lblPreis.Size = new System.Drawing.Size(53, 20);
+            this.lblPreis.TabIndex = 6;
+            this.lblPreis.Text = "Preis";
+            // 
+            // comboBoxKategorie
+            // 
+            this.comboBoxKategorie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxKategorie.FormattingEnabled = true;
+            this.comboBoxKategorie.Items.AddRange(new object[] {
+            "Obst",
+            "Gemüse",
+            "Fleisch",
+            "Fisch",
+            "Drogerie",
+            "Getränke"});
+            this.comboBoxKategorie.Location = new System.Drawing.Point(131, 101);
+            this.comboBoxKategorie.Name = "comboBoxKategorie";
+            this.comboBoxKategorie.Size = new System.Drawing.Size(276, 24);
+            this.comboBoxKategorie.TabIndex = 8;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(129, 190);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 37);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Speichern";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(286, 190);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(141, 37);
+            this.btnEdit.TabIndex = 10;
+            this.btnEdit.Text = "Bearbeiten";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(444, 190);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(141, 37);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Leeren";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(602, 190);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(141, 37);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Löschen";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridViewProduct
+            // 
+            this.dataGridViewProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProduct.Location = new System.Drawing.Point(0, 248);
+            this.dataGridViewProduct.Name = "dataGridViewProduct";
+            this.dataGridViewProduct.RowTemplate.Height = 24;
+            this.dataGridViewProduct.Size = new System.Drawing.Size(798, 205);
+            this.dataGridViewProduct.TabIndex = 13;
+            // 
+            // ProductsScreen
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(797, 453);
+            this.Controls.Add(this.dataGridViewProduct);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.comboBoxKategorie);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblPreis);
+            this.Controls.Add(this.lblKategorie);
+            this.Controls.Add(this.txtBoxMarke);
+            this.Controls.Add(this.lblMarke);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ProductsScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Produkte";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxMarke;
+        private System.Windows.Forms.Label lblMarke;
+        private System.Windows.Forms.Label lblKategorie;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblPreis;
+        private System.Windows.Forms.ComboBox comboBoxKategorie;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridViewProduct;
+    }
+}
